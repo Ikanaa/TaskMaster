65477a7d467aa1e4aec7ddf53a4fa2b97ee7863a
ikana <louis.saffre@gmail.com>
2025-04-09 11:33:18 +0200

    ✨ Add .net Maui app BASE

---
+++ App/Model/TaskDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Model;
+
+public class TaskDbContext : DbContext
+{
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(
+            "server=localhost;database=your_database_name;user=root;password=rootpassword;",
+            new MySqlServerVersion(new Version(10, 5, 0)) // Remplacez par la version de votre MariaDB
+        );
+    }
+}
