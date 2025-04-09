1c936a033110ae56f4fcd83780edda665db632d3
yoali <Yohan.ALI@etu.uca.fr>
2025-04-09 09:28:08 +0200

    Add app

---
+++ APP/taskMaster/Model/Projet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskMaster.Model
+{
+    public class Projet
+    {
+        private int id_pro { get; set; }
+        private string nom { get; set; }
+        private string description { get; set; }
+        private DateTime date_creation { get; set; }
+        private List<User> utilisateurs_projet { get; set; }
+        public Projet(int id, string nom, string description, List<User> utilisateurs, DateTime date)
+        {
+            this.id_pro = id;
+            this.nom = nom;
+            this.description = description;
+            this.date_creation = date;
+            this.utilisateurs_projet = utilisateurs;    
+        }
+    }
+}
---
+++ APP/taskMaster/Model/Task.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace taskMaster.Model
+{
+   
+    public class Task
+    {
+        private int id_tac{ get; set; }
+        private List<Task> sous_tache { get; set; }
+        private List<Commentaire> commentaires { get; set; }
+        private Projet projet { get; set; }
+        private User auteur { get; set; }
+        private User assignee { get; set; }
+        private string titre { get; set; }
+        private string description { get; set; }
+        private string statut { get; set; }
+        private string priorite { get; set; }
+        private string categorie { get; set; }
+        private string etiquettes { get; set; }
+        private DateTime date_creation { get; set; }
+
+
+        public Task(int id, string titre, string description, string statut, string priorite, string categorie, string etiquettes, DateTime date_creation)
+        {
+            this.id_tac = id;
+            this.titre = titre;
+            this.description = description;
+            this.statut = statut;
+            this.priorite = priorite;
+            this.categorie = categorie;
+            this.etiquettes = etiquettes;
+            this.date_creation = date_creation;
+
+        }
+
+
+
+    }
+}
